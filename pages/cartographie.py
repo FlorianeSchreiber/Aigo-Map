89ff6909d6ec50a534e684b707e6b3d7e9140ef1
FlorianeSchreiber <80046066+FlorianeSchreiber@users.noreply.github.com>
2024-05-02 23:43:37 +0200

    Update cartographie.py

--- pages/cartographie.py
+++ pages/cartographie.py
@@ -11,7 +11,7 @@ if 'Au restaurant' in options :
   Map.add_markers_from_xy(df_resto, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='blue', icon = "cutlery", layer_name="Au Restaurant")
 if 'Magasins d’alimentation' in options :
   df_alim = pd.read_csv("pages/aigo_alimentation.csv")
-  Map.add_markers_from_xy(df_alim, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='red', icon = "", layer_name="Magasins d’alimentation")
+  Map.add_markers_from_xy(df_alim, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='red', icon = "fork", layer_name="Magasins d’alimentation")
 
 Map.add_basemap("Terrain")
 Map.to_streamlit(height=700)
