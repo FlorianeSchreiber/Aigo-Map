45ad3b6205dd24b55aa17f06ccfa85937c9b5f03
FlorianeSchreiber <80046066+FlorianeSchreiber@users.noreply.github.com>
2024-05-02 21:51:17 +0200

    Update cartographie.py

--- pages/cartographie.py
+++ pages/cartographie.py
@@ -3,10 +3,6 @@ import geemap.foliumap as geemap
 
 st.title("Interactive Map")
 
-col1, col2 = st.columns([4, 1])
-options = list(geemap.basemaps.keys())
-index = options.index("OpenTopoMap")
-
 m = geemap.Map(center=(43.640212, 5.097115), zoom=12)
 m.add_point((43.641371, 5.097306), popup='Espigaou')
 m.to_streamlit(height=700)
