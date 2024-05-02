c1c64e8f56f5e619f3e7c436c56a1771c5784f46
FlorianeSchreiber <80046066+FlorianeSchreiber@users.noreply.github.com>
2024-05-02 22:40:06 +0200

    Update cartographie.py

--- pages/cartographie.py
+++ pages/cartographie.py
@@ -3,6 +3,5 @@ import pandas as pd
 
 Map = geemap.Map(center=(43.640212, 5.097115), zoom=14)
 df = pd.read_csv("aigo_pro.csv")
-#df = df.drop(['sov_a3', 'region'], axis=1)
 Map.add_markers_from_xy(df, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='white', icon = "pizza-slice")
 Map.to_streamlit(height=700)
