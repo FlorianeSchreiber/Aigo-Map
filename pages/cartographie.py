b8ef41fbb47a8acb252f6854063fdcbb93cb2d92
FlorianeSchreiber <80046066+FlorianeSchreiber@users.noreply.github.com>
2024-05-02 23:24:30 +0200

    Update cartographie.py

--- pages/cartographie.py
+++ pages/cartographie.py
@@ -6,6 +6,6 @@ Map = geemap.Map(center=(43.640212, 5.097115), zoom=14)
 
 
 df = pd.read_csv("pages/aigo_pro.csv")
-Map.add_markers_from_xy(df, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='blue', icon = "pizza-slice")
+Map.add_markers_from_xy(df, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='blue', icon = "pizza-slice", layer_name="Restaurants")
 Map.add_basemap("Terrain")
 Map.to_streamlit(height=700)
