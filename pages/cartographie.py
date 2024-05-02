b423e51e784b19f2baf84a305618aada8297d008
FlorianeSchreiber <80046066+FlorianeSchreiber@users.noreply.github.com>
2024-05-02 22:37:38 +0200

    Update cartographie.py

--- pages/cartographie.py
+++ pages/cartographie.py
@@ -1,8 +1,8 @@
-import streamlit as st
 import geemap.foliumap as geemap
+import pandas as pd
 
-st.title("Interactive Map")
-
-m = geemap.Map(center=(43.640212, 5.097115), zoom=12)
-m.add_point((43.641371, 5.097306), popup='Espigaou')
+Map = geemap.Map(center=(43.640212, 5.097115), zoom=14)
+df = pd.read_csv("Aigo_Map/aigo_pro.csv")
+#df = df.drop(['sov_a3', 'region'], axis=1)
+Map.add_markers_from_xy(df, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='white', icon = "pizza-slice")
 m.to_streamlit(height=700)
