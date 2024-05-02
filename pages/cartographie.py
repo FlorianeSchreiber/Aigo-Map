2872caad7fbe2ead0a114c6f8af75e8ef6238080
FlorianeSchreiber <80046066+FlorianeSchreiber@users.noreply.github.com>
2024-05-02 23:34:27 +0200

    Update cartographie.py

--- pages/cartographie.py
+++ pages/cartographie.py
@@ -4,6 +4,8 @@ import streamlit as st
 
 Map = geemap.Map(center=(43.640212, 5.097115), zoom=14)
 
+st.multiselect("Les catégories de Commerces", ['Magasins d’alimentation', 'Au restaurant', 'A La Ferme', 'Traiteurs' , 'Culture', 'Arts de la maison 
+', 'Santé soins et soins alternatifs', 'Vêtements', 'Beauté', 'Services', 'Construction', 'Sport', 'Gîte'], ['Magasins d’alimentation', 'Au restaurant'])
 
 df = pd.read_csv("pages/aigo_pro.csv")
 Map.add_markers_from_xy(df, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='blue', icon = "pizza-slice", layer_name="Restaurants")
