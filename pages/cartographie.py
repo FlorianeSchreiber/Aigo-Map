f1e17d5276c01185f0163848f4a9290404b6b09a
FlorianeSchreiber <80046066+FlorianeSchreiber@users.noreply.github.com>
2024-05-02 23:52:29 +0200

    Update cartographie.py

--- pages/cartographie.py
+++ pages/cartographie.py
@@ -8,10 +8,10 @@ options = st.multiselect("Les catégories de Commerces", ['Magasins d’alimenta
 
 if 'Au restaurant' in options :
   df_resto = pd.read_csv("pages/aigo_restaurants.csv")
-  Map.add_markers_from_xy(df_resto, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='black', border_width=1, icon = "", layer_name="Au Restaurant", background_color = "#CCCCFF")
+  Map.add_markers_from_xy(df_resto, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='black', border_width=1, icon = "", layer_name="Au Restaurant", background_color = "#7a7a99")
 if "Magasins d’alimentation" in options :
   df_alim = pd.read_csv("pages/aigo_alimentation.csv")
-  Map.add_markers_from_xy(df_alim, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='red', icon = "", layer_name="Magasins d’alimentation", background_color = "red")
+  Map.add_markers_from_xy(df_alim, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='black', border_width=1, icon = "", layer_name="Magasins d’alimentation", background_color = "997A7A")
 
 Map.add_basemap("Terrain")
 Map.to_streamlit(height=700)
