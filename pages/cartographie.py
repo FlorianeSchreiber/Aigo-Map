03d99655934074510b8c6860e818c420e5ca5cdc
FlorianeSchreiber <80046066+FlorianeSchreiber@users.noreply.github.com>
2024-05-02 23:48:58 +0200

    Update cartographie.py

--- pages/cartographie.py
+++ pages/cartographie.py
@@ -8,7 +8,7 @@ options = st.multiselect("Les catégories de Commerces", ['Magasins d’alimenta
 
 if 'Au restaurant' in options :
   df_resto = pd.read_csv("pages/aigo_restaurants.csv")
-  Map.add_markers_from_xy(df_resto, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='blue', icon = "cutlery", layer_name="Au Restaurant")
+  Map.add_markers_from_xy(df_resto, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='blue', icon = "", layer_name="Au Restaurant", background_color = "blue")
 if "Magasins d’alimentation" in options :
   df_alim = pd.read_csv("pages/aigo_alimentation.csv")
   Map.add_markers_from_xy(df_alim, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='red', icon = "", layer_name="Magasins d’alimentation", background_color = "red")
