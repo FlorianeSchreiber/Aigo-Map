797035c0bdc8e65d07be6c5289200740e868090c
FlorianeSchreiber <80046066+FlorianeSchreiber@users.noreply.github.com>
2024-05-02 22:41:42 +0200

    Update cartographie.py

--- pages/cartographie.py
+++ pages/cartographie.py
@@ -2,6 +2,6 @@ import geemap.foliumap as geemap
 import pandas as pd
 
 Map = geemap.Map(center=(43.640212, 5.097115), zoom=14)
-df = pd.read_csv("aigo_pro.csv")
+df = pd.read_csv("pages/aigo_pro.csv")
 Map.add_markers_from_xy(df, x="longitude", y="latitude", popup=["Nom", "Adresse"], icon_shape = "circle", border_color='white', icon = "pizza-slice")
 Map.to_streamlit(height=700)
